a12045f55859155d1f1a4fe1f7caf407f7d91809
Yi Pang <pangyibobo@gmail.com>
2015-11-23 16:57:49 -0800

    final

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # portfolio-
+http://students.washington.edu/yip3/info343/portfolio-/
+<br/>
+I just receive help from Michael
+<br/>
+about 12 hours
+<br/>
+I just messed up the order of libraries with ui router
--- js/main.js
+++ js/main.js
@@ -25,12 +25,30 @@ myApp.config(function($stateProvider) {
     })
 })
 
-// Landing page controller: define $scope.number as a number
 myApp.controller('HomeController', function($scope) {
-    $scope.words = 6;
+    $scope.intro = "This is my first personal portfolio"
 })
 
-// About page controller: define $scope.about as a string
+myApp.controller('ProjectsController', function($scope) {
+    $scope.url1 = 'img/mulan.png'
+    $scope.url2 = 'img/coffee.png'
+    $scope.url3 = 'img/spotify.png'
+    $scope.urlm = 'http://students.washington.edu/yip3/info343/git-started/'
+    $scope.urlc = 'http://students.washington.edu/yip3/info343/dawg-coffee/'
+    $scope.urls = 'http://students.washington.edu/yip3/info343/spotify-template/'
+})
+
 myApp.controller('AboutController', function($scope) {
-    $scope.about = "Some string";
+    $scope.aboutme = 'Yi Pang, a informatics major student. This is the first time learning clident-side web development';
+})
+
+myApp.controller('ContactController', function($scope) {
+    $scope.mail = "pangyibobo@gmail.com"
+})
+
+myApp.controller('myCtrl', function($scope, $http){
+    $http.get('list.json').then(function(response) {
+        $scope.data = response.data
+        console.log($scope.data)
+    })
 })
